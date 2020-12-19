6997ffff6d23cf10a1cb1529531e2652e52e4d7d
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-19 02:33:24 -0600

    basic button click function.

--- js/script_tmp.js
+++ js/script_tmp.js
@@ -1,5 +1,22 @@
 const init = () => {
-  console.log('Enter chatroom');
+  // console.log('Enter chatroom');
+
+  const inputText = document.getElementById('tbx-input');
+  const sendButton = document.getElementById('btn-send');
+
+  sendButton.addEventListener('click', () => {
+    const message = inputText.value;
+    sendMessage(message);
+    inputText.value = '';
+  });
 };
+
+function sendMessage(message) {
+  if (message == '') {
+    alert('input box can not be empty');
+  } else {
+    console.log(message);
+  }
+}
 
 init();
